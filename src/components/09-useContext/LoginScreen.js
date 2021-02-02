4101b55f0f09e43734515501cf9b16cc2ea2f3da
CarlosGRJ <carlosgrj@outlook.com>
2021-02-02 13:59:35 -0600

    Fin sección 12}

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+  // 1. Obtener la referencia al context
+  const { setUser } = useContext(UserContext);
+
   return (
     <div>
       <h1>LoginScreen</h1>
       <hr />
+
+      <button
+        className='btn btn-primary'
+        onClick={() =>
+          setUser({
+            id: 123,
+            name: 'Carlos',
+          })
+        }>
+        Login
+      </button>
     </div>
   );
 };
---
+++ src/tests/hooks/useCounter.test.js
@@ -0,0 +1,59 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useCounter } from '../../hooks/useCounter';
+
+describe('Pruebas en useCounter', () => {
+  test('debe de retornar valores por defecto', () => {
+    const { result } = renderHook(() => useCounter());
+    // console.log('result ', result.current);
+    expect(result.current.counter).toBe(10);
+    expect(typeof result.current.increment).toBe('function');
+    expect(typeof result.current.decrement).toBe('function');
+    expect(typeof result.current.reset).toBe('function');
+  });
+
+  test('debe de tener el counter en 100', () => {
+    const { result } = renderHook(() => useCounter(100));
+    // console.log('result ', result.current);
+    expect(result.current.counter).toBe(100);
+  });
+
+  test('debe de incrementar el counter en 1', () => {
+    const { result } = renderHook(() => useCounter(100));
+    // console.log('result ', result.current);
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+      //   reset();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(101);
+  });
+
+  test('debe de decrementar el counter en 1', () => {
+    const { result } = renderHook(() => useCounter(100));
+    // console.log('result ', result.current);
+    const { decrement } = result.current;
+
+    act(() => {
+      decrement();
+      //   reset();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(99);
+  });
+
+  test('debe de establecer el valor en 100', () => {
+    const { result } = renderHook(() => useCounter(100));
+    // console.log('result ', result.current);
+    const { decrement } = result.current;
+    const { reset } = result.current;
+
+    act(() => {
+      decrement();
+      reset();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(100);
+  });
+});
